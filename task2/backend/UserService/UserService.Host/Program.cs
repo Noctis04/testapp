037d013f420f1636336bced41cd3a5ec10f97c8c
Noctis04 <137399833+Noctis04@users.noreply.github.com>
2023-07-06 11:46:26 +0300

    Add files via upload

---
+++ task2/backend/UserService/UserService.Host/Program.cs
@@ -0,0 +1,27 @@
+using UserService.Host.Routes;
+using UserService.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Environment.IsDevelopment()
+    ? builder.Configuration.GetConnectionString("DefaultConnection")
+    : Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+builder.Services.AddBusinessLogic(builder.Configuration, connectionString!);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.AddUserRouter();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.Run();
